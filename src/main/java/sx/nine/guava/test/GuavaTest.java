85f6912399df530f33da59bdf01e4c25a429ce70
Azure0919 <psx0919@foxmail.com>
2021-03-30 23:20:27 +0800

    pull

--- src/main/java/sx/nine/guava/test/GuavaTest.java
+++ src/main/java/sx/nine/guava/test/GuavaTest.java
@@ -1,6 +1,8 @@
 package sx.nine.guava.test;
 
 
+import java.io.FileInputStream;
+
 /**
  * @Author NineEr
  * @Description //
@@ -8,13 +10,6 @@ package sx.nine.guava.test;
  **/
 public class GuavaTest {
     public static void main(String[] args) {
-
-        /*第一题：
-        某国货币系统包含面值1元，4元，16元，64元共计4种硬币，以及面值1024元的纸币。
-        现在某人使用1024元的纸币购买了一件价格为N(0≤N≤1024)的商品。
-        请问最少他会收到多少枚硬币?
-            求解思路：贪心算法*/
-
 
 
     }
