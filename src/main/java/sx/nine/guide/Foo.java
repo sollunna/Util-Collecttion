62cc8a16a59f1c2342c610e4a4145cb0e3ccb3a0
Azure0919 <psx0919@foxmail.com>
2021-04-01 17:32:24 +0800

    pull

--- src/main/java/sx/nine/guide/Foo.java
+++ src/main/java/sx/nine/guide/Foo.java
@@ -7,6 +7,27 @@ package sx.nine.guide;
  * @since 1.0
  * @version 1.0
  */
+
+/**
+ *
+ *
+ * 我们提供了一个类：
+ *
+ * public class Foo {
+ *   public void first() { print("first"); }
+ *   public void second() { print("second"); }
+ *   public void third() { print("third"); }
+ * }
+ * 三个不同的线程 A、B、C 将会共用一个 Foo 实例。
+ *
+ * 一个将会调用 first() 方法
+ * 一个将会调用 second() 方法
+ * 还有一个将会调用 third() 方法
+ * 请设计修改程序，以确保 second() 方法在 first() 方法之后被执行，third() 方法在 second() 方法之后被执行。
+ *
+ * 来源：力扣（LeetCode）
+ * @throws
+*/
 public class Foo {
 
   private int count = 0;
