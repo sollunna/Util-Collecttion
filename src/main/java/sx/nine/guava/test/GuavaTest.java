9f968bac3c3f12fa5f2b65b66ec7487b6c218384
Azure0919 <psx0919@foxmail.com>
2021-04-04 10:13:02 +0800

    pull

--- src/main/java/sx/nine/guava/test/GuavaTest.java
+++ src/main/java/sx/nine/guava/test/GuavaTest.java
@@ -1,6 +1,8 @@
 package sx.nine.guava.test;
 
 
+import javax.swing.*;
+import java.io.*;
 
 /**
  * @Author NineEr
@@ -8,8 +10,24 @@ package sx.nine.guava.test;
  * @Date $ $
  **/
 public class GuavaTest {
-    public static void main(String[] args)  {
+    public static void main(String[] args) throws IOException {
+        try
+        {
+            String fileName= "hellojava";
+            File in=new File(fileName);
+            FileReader reader=new FileReader(in);
+            //读取文本
+            char[] count=new char[(int)in.length()];
+            reader.read(count);
+            //统计单词数量
+            String s=String.valueOf(count);
+            String[ ] words=s.replaceAll("[^a-zA-Z]+"," ").trim( ).split(" ");
 
-
+            System.out.println("字符数量："+count.length+"\n单词数量："+words.length);
+            reader.close();
+        }
+        catch(IOException exception) {
+            exception.printStackTrace();
+        }
     }
 }
